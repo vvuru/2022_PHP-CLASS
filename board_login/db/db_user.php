9fd9514fd25d43008820302661bbd00c81cd6a30
vvuru <86989509+vvuru@users.noreply.github.com>
2022-04-22 17:17:28 +0900

    22/04/22 8th

---
+++ board_login/db/db_user.php
@@ -0,0 +1,9 @@
+<?php
+    include_once "db.php";
+
+    function join(param)
+    {
+        $conn=get_conn();
+        $sql=
+        "INSERT INTO t_user (uid,upw,nm,
+    }
---
+++ board_login/join_proc.php
@@ -0,0 +1,12 @@
+<?php
+    $uid=$_POST['uid'];
+    $upw=$_POST['upw'];
+    $confirm_upw=$_POST['confirm_upw'];
+    $nm=$_POST['nm'];
+    $gender=$_POST['gender'];
+
+    echo "uid: ".$uid."<br>";
+    echo "upw: ".$upw."<br>";
+    echo "confirm_upw: ".$confirm_upw."<br>";
+    echo "nm: ".$nm."<br>";
+    echo "gender: ".$gender."<br>";
